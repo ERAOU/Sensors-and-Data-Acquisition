bcc7fce87ec79718df2a88bc201a01e58935adfd
falshubeilat <131100941+falshubeilat@users.noreply.github.com>
2023-08-10 16:05:58 -0400

    Logan driver display test

---
+++ Hall effect Sensor CAN implementation/LOGAN_TEST__/LOGAN_TEST__.ino
@@ -0,0 +1,33 @@
+void setup() {
+  // Initialize the serial communication at 9600 baud
+  Serial.begin(115200);
+
+  // Initialize the random seed
+  randomSeed(analogRead(0));
+
+  // Create a character array to hold the 20 hexadecimal numbers
+  char hexString[20]; // 20 characters plus the null terminator
+
+  // First four characters are always the same
+  hexString[0] = 'A';
+  hexString[1] = 'B';
+  hexString[2] = 'C';
+  hexString[3] = 'D';
+
+  // Generate the remaining 16 random hexadecimal characters
+  for(int i = 4; i < 20; i++) {
+    int randomValue = random(16);
+    if (randomValue < 10) {
+      hexString[i] = '0' + randomValue;
+    } else {
+      hexString[i] = 'A' + (randomValue - 10);
+    }
+  }
+
+  // Print the hexadecimal string to the serial monitor
+  Serial.println(hexString);
+}
+
+void loop() {
+  // Nothing to do here
+}
